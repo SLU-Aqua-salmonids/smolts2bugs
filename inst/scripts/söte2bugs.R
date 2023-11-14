95b80c90baf5b8d5f7061ad091fe30a94867fd49
Anders Kagervall <Anders.Kagervall@slu.se>
2023-11-14 09:55:47 +0100

    Use files in //storage-dh.slu.se/restricted$/Sötebasen/Exporter as input

--- inst/scripts/söte2bugs.R
+++ inst/scripts/söte2bugs.R
@@ -1,31 +1,34 @@
 ###
 ## Script to transform a smoltdata exported from Sötebasen into formats usable
 ## as input to mark/recapture models.
-##  Time-stamp: <2023-10-18 12:50:06 ankag>
+##  Time-stamp: <2023-11-10 12:50:06 ankag>
 ##
 
 library(dplyr)
 library(readxl)
 library(writexl)
 library(lubridate)
-library(Smoltreg)
-library(smolts2bugs)
-##
-sweet_file <- file.choose() # Get filenamew interactivly
-#sweet_file <- "c:/ExportSötebasen/AA02_MarkRecapSmolts_Query.xlsx" # Use hardcoded name
-d <- read_excel(sweet_file)
-river <- d$VattenNamn[1]
-year <- year(d$AnstrDatumStart[1])
-## Choose species here
+library(Smoltreg) # https://github.com/kagervall/Smoltreg.git
+library(smolts2bugs) # https://github.com/kagervall/smolts2bugs.git
+###
+## Choose river, year and species to process
+river <- "Mörrumsån"
+year <- year(Sys.Date())
 species <- "Lax"
 #species <- "Öring"
+###
+## Location input files are set with smolts2bugs::sdb(). By default files
+## automatically exported to Sölab file server. See ?sdb for options.
+## E.g. to read files from current current directory (instead of default)
+## run sdb(root_folder = ".")
 
-RESULTDIR <- paste0("SMOLTS2_", river, "_", year)
+RESULTDIR <- paste0("SMOLTS_", river, "_", year)
 SPECIESDIR <- file.path(RESULTDIR, species)
+message(paste0("Results will be saved in folder: ", SPECIESDIR))
 minlength <- Smoltreg_limits()$minlength
 maxlength <- Smoltreg_limits()$maxlength
-## dates <- read_excel(sweet_file, sheet = "Ansträngning") %>%
-dates <- d[1, ] %>%
+
+dates <-  sdb_read_occasions(VattenNamn = river, Year = year) %>%
   mutate(N_days = 1 + as.numeric(AnstrDatumSlut - AnstrDatumStart),
          start_day_of_year = as.POSIXlt(AnstrDatumStart)$yday,
          start_date = AnstrDatumStart,
@@ -34,9 +37,7 @@ dates <- d[1, ] %>%
 
 
 # Get all rows for a species and remove too small and too fish
-#fish <- read_excel(sweet_file, sheet = "Individ") %>%
-fish <- d %>%
-  filter(Art == species) %>%
+fish <- sdb_read_catch_recatch(Art = species, VattenNamn = river, Year = year) %>%
   filter(between(Längd1, minlength, maxlength) | is.na(Längd1)) %>%
   mutate(day_of_year = as.POSIXlt(FångstDatum)$yday, pittag = MärkeNr,
          event = case_when(Behandling == "Utsatt" ~ Smoltreg::event$CAUGHT,
@@ -58,12 +59,6 @@ tagged <- fish %>% filter(event == Smoltreg::event$MARKED) %>%
   select(pittag, day_of_year, species) %>%
   rename(capture_day = day_of_year) %>%
     mutate(capture_day = capture_day - dates$start_day_of_year + 1)
-####
-## Create a data frame "recaptured" with all recaptures
-recaptured <- fish %>% filter(event == Smoltreg::event$RECAPTURED) %>%
-  select(pittag, day_of_year) %>%
-  rename(recapture_day = day_of_year) %>%
-  mutate(recapture_day = recapture_day - dates$start_day_of_year + 1)
 
 ####
 ## Create a data frame "captured" with fish that was captured but NOT
@@ -78,6 +73,12 @@ captured <- fish %>%
            capture_day = capture_day - dates$start_day_of_year + 1,
            recapture_day = NA,
            marked = FALSE)
+####
+## Create a data frame "recaptured" with all recaptures
+recaptured <- fish %>% filter(event == Smoltreg::event$RECAPTURED) %>%
+  select(pittag, day_of_year) %>%
+  rename(recapture_day = day_of_year) %>%
+  mutate(recapture_day = recapture_day - dates$start_day_of_year + 1)
 
 all_captures <- tagged %>%
     left_join(recaptured, by = "pittag") %>%
@@ -85,16 +86,13 @@ all_captures <- tagged %>%
     bind_rows(captured)
 ####
 ## Read water temp and water level.
-#envfile <- file.choose()
-envfile <- "c:/ExportSÃ¶tebasen/AA03_Temperatur_Query.xlsx"
-#envdata <- read_excel(sweet_file, sheet = "Temperatur") %>%
-envdata <- read_excel(envfile) %>%
+envdata <- sdb_read_catch_envir(VattenNamn = river, Year = year) %>%
   mutate(dnum = as.POSIXlt(Datum)$yday, date = Datum,
          w_level = WL, w_temp = WT) %>%
   select(dnum, date, w_level, w_temp) %>%
-    filter(between(date, dates$start_date, dates$stop_date)) # Remove measures out
+    filter(between(date, dates$start_date, dates$stop_date)) # Only keep dates within this trapping period
 #####
-## TODO. Set missing days. Missing dates aren't in SÃ¶tebasen (yet?).
+## TODO. Set missing days. Missing dates aren't in Sötebasen (yet?).
 ## See tab Metadata in the Smoltreg-file if we have missing dates and enter them
 ## below.
 ## Missing days should be entered as day number where dates$start_date = 1
@@ -107,18 +105,17 @@ missing_days  <- NULL
 ## be used as Data2 in the Blackbox smolt model.
 ## Save the formated data in excel-file and also in an Rdump file.
 ## The Rdump contains some useful metadata besides the Data2 structure.
-Data2  <- format_Data2(fish=all_captures, env=envdata, ndays=dates$N_days,
+Data2  <- smolts2bugs::format_Data2(fish=all_captures, env=envdata, ndays=dates$N_days,
                        missing_days = missing_days)
-save_bugsdata(Data2, path = SPECIESDIR)
-save_Rdatadump(Data2, river = river, species = species,
+smolts2bugs::save_bugsdata(Data2, path = SPECIESDIR)
+smolts2bugs::save_Rdatadump(Data2, river = river, species = species,
                startd = dates$start_date, stopd = dates$stop_date,
                missing_days = missing_days, path = SPECIESDIR)
 
-##
-## Save an markdown file that will present the results later when the model
-## have been run.
+## Save a draft of the reults.Rmd template in the output directory. When models
+## have produced rresults knit this draft to get a report.
 if (require(SmoltReports)) {
   rmarkdown::draft(file.path(SPECIESDIR, "results.Rmd"),
-                 template = "smolt-estimates-blackbox",
-                 package = "SmoltReports", edit = FALSE)
+                   template = "smolt-estimates-blackbox",
+                   package = "SmoltReports", edit = FALSE)
 }
